5c6fe356d0bf16a73609d29a12d148e162d97808
megupatil <megupatil@gmail.com>
2025-07-18 19:28:41 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.9"
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.20"
@@ -12,9 +16,41 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.5"
     }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.4"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
 }
 
 provider "aws" {
   region = var.aws_region
+}
+
+# Provider to install Helm charts on the EKS cluster
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.id]
+      command     = "aws"
+    }
+  }
+}
+
+# Provider to apply Kubernetes manifests (like the IngressClass)
+provider "kubernetes" {
+  host                   = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.id]
+    command     = "aws"
+  }
 }
